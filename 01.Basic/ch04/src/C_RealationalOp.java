4fd650acbe4a2c61ad68f1248310fea9e6979131
kangseungmin@kosmo <durusk93@gmail.com>
2020-02-06 16:02:49 +0900

    ch05 추가

---
+++ 01.Basic/ch04/src/C_RealationalOp.java
@@ -0,0 +1,12 @@
+
+public class C_RealationalOp {
+
+	public static void main(String[] args) {
+		System.out.println("3 >= 2 : " + (3 >= 2));
+		System.out.println("3 <= 2 : " + (3 <= 2));
+		
+		// 비교 연산시 형 변환이 일어난다.
+		System.out.println("7.0 == 7 : " + (7.0 == 7));
+		System.out.println("7.0 != 7 : " + (7.0 != 7));
+	}
+}

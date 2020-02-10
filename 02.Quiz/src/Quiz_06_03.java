acb4e44eeae14e767f37c5e37333cd39e88f16e9
seungminkang <durusk93@gmail.com>
2020-02-10 10:44:38 +0900

    코드 수정 및 주석 추가

--- 02.Quiz/src/Quiz_06_03.java
+++ 02.Quiz/src/Quiz_06_03.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class Quiz_06_03 {
 
 	public static void main(String[] args) {
+		//정수 하나 입력
+		//그 수의 팩토리얼 함수의 결과를 출력
+		// <-- while 문 사용
+		// 5 : 5 * 4 * 3 * 2 * 1
+		
+		//사용자한테 한 개의 정수를 입력받아라
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("숫자를 입력하세요. ");
@@ -11,9 +17,12 @@ public class Quiz_06_03 {
 		int fac = 1;
 		
 		while(num > 0) {
+			if(num == 1) {
+				System.out.println(num + " = " + fac);
+			}
+			System.out.print(num + " * ");
 			fac = fac * num;
 			num--;
 		}
-		System.out.println(ini + "!의 값은 " + fac + " 입니다.");
 	}
 }

c1be5c71e79cfd35d12a92bacd136d00177766e1
seungminkang <durusk93@gmail.com>
2020-02-10 11:15:55 +0900

    예제 추가

---
+++ 01.Basic/ch06/src/Ex1.java
@@ -0,0 +1,31 @@
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		// 1씩 증가
+		for (int i=0; i<100 ; i=i+1) {
+		}
+		
+		// 2씩 증가
+		for (int i = 0; i<100; i=i+2) {
+		}
+		
+		// 조건에 따라 증가
+		int n = 50;
+		for (int i=0; i<100; ) {
+			if(n > 10) {
+				i = i + 3;
+			} else {
+				i = i + 3;
+			}
+		}
+		
+		// 1씩 감소
+		for(int i=9; i>=0; i--) {
+		}
+		
+		// 2씩 감소
+		for(int i=9; i>=0; i=i-2) {
+		}
+	}
+}

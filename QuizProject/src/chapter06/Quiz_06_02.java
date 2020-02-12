89080e386b0c2a88085679422a61badf260c78d6
seungminkang@kosmo <durusk93@gmail.com>
2020-02-12 09:24:20 +0900

    int형을 double형으로 변경, 평균 점수가 소수점 2자리까지 나오도록 변경

--- QuizProject/src/chapter06/Quiz_06_02.java
+++ QuizProject/src/chapter06/Quiz_06_02.java
@@ -8,17 +8,17 @@ public class Quiz_06_02 {
 		
 		System.out.println("평균 점수 계산 프로그램입니다.");
 		System.out.print("국어 점수를 입력하세요 : ");
-		int kor = sc.nextInt();
+		double kor = sc.nextInt();
 		
 		System.out.print("영어 점수를 입력하세요 : ");
-		int eng = sc.nextInt();
+		double eng = sc.nextInt();
 		
 		System.out.print("수학 점수를 입력하세요 : ");
-		int mat = sc.nextInt();
+		double mat = sc.nextInt();
 		
-		int avg = (kor + eng + mat) / 3;
+		double avg = (kor + eng + mat) / 3;
 		
-		System.out.println("평균 점수는 " + avg + " 입니다.");
+		System.out.printf("평균 점수는 %.2f 입니다.\n", avg);
 		if(avg >= 90) {
 			System.out.println("당신의 성적은 A 입니다.");
 		}

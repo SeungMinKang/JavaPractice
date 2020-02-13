382a63fb93f7f0b4bee3a1557cb14729a84da60c
seungminkang@kosmo <durusk93@gmail.com>
2020-02-13 16:24:53 +0900

    다른 접근 방법의 로직 추가

--- QuizProject/src/Game/QuizAZ.java
+++ QuizProject/src/Game/QuizAZ.java
@@ -13,7 +13,32 @@ public class QuizAZ {
 		for(int i = 0; i <= 9; i++) {
 			int j = sum - i;
 			System.out.printf(" %d%d\n+%d%d\n---\n 99",i, j, j, i);
-			System.out.printf("\tA: %d\tZ: %d\n\n", i, j);
+			System.out.printf("\tA = %d\tZ = %d\n\n", i, j);
 		}
+		
+//		for(int i=0; i<100; i++) {
+//			// i = 15
+//			int A = i / 10;
+//			int Z = i % 10;
+//			
+//			if(A + Z == 9){
+//				System.out.printf("A = %d, Z = %d \n", A, Z);				
+//			}
+//		}
+		
+//		int A = 0, Z = 0;
+//		
+//		for(A = 0; A < 10; A++) {
+//			for(Z = 0; Z < 10; Z++) {
+//				int num1 = A * 10 + Z;
+//				int num2 = Z * 10 + A;
+//				
+//				if(A + Z == 9) {
+//					if(A != Z) {
+//						System.out.println(num1 + " " + num2);
+//					}
+//				}
+//			}
+//		}
 	}
 }
--- QuizProject/src/Game/QuizRandom.java
+++ QuizProject/src/Game/QuizRandom.java
@@ -16,15 +16,17 @@ public class QuizRandom {
 				System.out.printf("%d%d%d", num1,num2,num3);
 				break;
 			}
-//			if(num1 == num2) {
-//				if(num1 == num3) {
-//					System.out.printf("%d%d%d 등 안됨 : %d이(가) 세개임", num1, num2, num3, num1);
-//					break;
-//				}else {
-//					System.out.printf("%d%d%d 등 안됨 : %d이(가) 두개임", num1, num2, num3, num1);
-//					break;
-//				}
-//			}
+			if(num1 == num2) {
+				if(num1 == num3) {
+					System.out.printf("%d%d%d 등 안됨 : %d이(가) 세개임", num1, num2, num3, num1);
+					break;
+				}else {
+					System.out.printf("%d%d%d 등 안됨 : %d이(가) 두개임", num1, num2, num3, num1);
+					break;
+				}
+			}
+		}
+		
 //			else if(num1 == num3) {
 //				if(num1 == num2) {
 //					System.out.printf("%d%d%d 등 안됨 : %d이(가) 세개임", num1, num2, num3, num1);
@@ -43,6 +45,26 @@ public class QuizRandom {
 //					break;
 //				}
 //			}
-		}
+			
+			
+		
+//		int n = 0;
+//		while(true) {
+//			int num = rnd1.nextInt(900) + 100;	//(0 ~ 899) + 100
+//			int n1 = num / 100;
+//			int t1 = num % 100;
+//			int n2 = t1 / 10;
+//			int n3 = t1 % 10;
+//			
+//			if((n1 != n2) && (n2 != n3) && (n3 != n1)) {
+//				System.out.printf("%d%d%d", n1, n2 ,n3);
+//				break;
+//			}
+//			// 무한 루프를 방지하기 위한 로직
+//			// 이상 없음을 확인한 후 제거해도 된다.
+//			if(n > 50)
+//				break;
+//			n++;
+//		}
 	}
 }

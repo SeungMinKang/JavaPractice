eca72258013517e8df45a1d6aea7acb81d803925
KangSeungMin <durusk93@gmail.com>
2020-02-16 23:08:32 +0900

    QuizHighLow 추가

---
+++ QuizProject/src/Game/QuizHighLow.java
@@ -0,0 +1,41 @@
+package Game;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class QuizHighLow {
+
+	public static void main(String[] args) {
+		Random rnd = new Random();
+		Scanner sc = new Scanner(System.in);
+		int[] you = new int[6];
+
+		while(true) {
+			int com = rnd.nextInt(101);
+			System.out.println("0과 100 사이의 값 중 하나를 생각하겠습니다.");
+			System.out.println("그 숫자를 6회 안에 맞추십시오.");
+			for(int i = 0; i < 6; i++) {
+				System.out.print("어떤 숫자일 것 같나요?: ");
+				you[i] = sc.nextInt();
+				if(you[i] == com) {
+					System.out.println(com + "은(는) 정답입니다! 축하합니다!");
+					break;
+				}
+				else if(you[i] < com) {
+					System.out.println(you[i] + "은(는) 정답보다 작은 숫자입니다.");
+				}else {
+					System.out.println(you[i] + "은(는) 정답보다 큰 숫자입니다.");
+				}
+			}
+			System.out.println("정답은 " + com + "였습니다!");
+			System.out.println("기회를 모두 소진하셨습니다.");
+			System.out.print("게임을 재시작 하시겠습니까? (y/n)");
+			String str = sc.next();
+			if(!str.equalsIgnoreCase("y")) {
+				System.out.println("게임을 종료합니다.");
+				break;
+			}
+			System.out.println();
+		}
+	}
+}

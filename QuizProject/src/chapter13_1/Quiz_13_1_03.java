f10cd02a7166d7f038e5108bd23edbb1180e52eb
KangSeungMin <durusk93@gmail.com>
2020-02-16 23:08:17 +0900

    static int 변수 추가, for 카운트의 조건을 num.length에서 oddcount, evencount로 변경

--- QuizProject/src/chapter13_1/Quiz_13_1_03.java
+++ QuizProject/src/chapter13_1/Quiz_13_1_03.java
@@ -3,6 +3,9 @@ package chapter13_1;
 import java.util.Scanner;
 
 public class Quiz_13_1_03 {
+	private static int oddcount = 0;
+	private static int evencount = 0;
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -12,6 +15,15 @@ public class Quiz_13_1_03 {
 			System.out.print("정수를 입력하세요: ");
 			num[i] = sc.nextInt();
 		}
+		
+		for(int i = 0; i < num.length; i++) {
+			if(num[i] % 2 != 0) {
+				oddcount = oddcount + 1;			
+			}else {
+				evencount = evencount + 1;
+			}
+		}
+		
 		odd(num);
 		
 		System.out.println();
@@ -23,7 +35,7 @@ public class Quiz_13_1_03 {
 	
 	public static void odd(int[] num) {
 		int j = 0;
-		int[] odd = new int[num.length];
+		int[] odd = new int[oddcount];
 		for(int i = 0; i < num.length; i++) {
 			if(num[i] % 2 != 0) {
 				odd[j] = num[i];
@@ -31,14 +43,14 @@ public class Quiz_13_1_03 {
 			}
 		}
 		System.out.print("홀수: ");
-		for(int i = 0; i < num.length; i++) {
+		for(int i = 0; i < oddcount; i++) {
 			System.out.print(odd[i] + " ");
 		}
 	}
 	
 	public static void even(int[] num) {
 		int j = 0;
-		int[] even = new int[num.length];
+		int[] even = new int[evencount];
 		for(int i = 0; i < num.length; i++) {
 			if(num[i] % 2 == 0) {
 				even[j] = num[i];
@@ -46,7 +58,7 @@ public class Quiz_13_1_03 {
 			}
 		}
 		System.out.print("짝수: ");
-		for(int i = 0; i < num.length; i++) {
+		for(int i = 0; i < evencount; i++) {
 			System.out.print(even[i] + " ");
 		}
 	}

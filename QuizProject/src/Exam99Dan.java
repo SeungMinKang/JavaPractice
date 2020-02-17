0579c74bc808f9b16c3b0ca0a1d3ca2beffe15a2
seungminkang@kosmo <durusk93@gmail.com>
2020-02-17 19:46:27 +0900

    구구단 추가

---
+++ QuizProject/src/Exam99Dan.java
@@ -0,0 +1,10 @@
+public class Exam99Dan {
+	public static void main(String[] args) {
+		for(int i =9; i>1; i--) {			
+			for(int j = 9; j > 0; j--) {
+				System.out.print(i + " X " + j + " = " + (i*j) + "\t");
+			}
+			System.out.println();		
+		}
+	}
+}

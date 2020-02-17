ebced6d0551332dd1c7030b5bdf1713d23243889
seungminkang@kosmo <durusk93@gmail.com>
2020-02-17 11:20:25 +0900

    class 형태로 변환 및 로직 수정

--- QuizProject/src/Game/QuizHighLow.java
+++ QuizProject/src/Game/QuizHighLow.java
@@ -4,17 +4,28 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class QuizHighLow {
+	public static void main(String[] args) {		
+		GuessIt guessit = new GuessIt();
+		
+		guessit.gameSet();
+	}
+}
 
-	public static void main(String[] args) {
-		Random rnd = new Random();
-		Scanner sc = new Scanner(System.in);
-		int[] you = new int[6];
-
+class GuessIt {
+	Random rnd = new Random();
+	Scanner sc = new Scanner(System.in);
+	
+	int[] you = new int[6];
+	int com = rnd.nextInt(101);
+	int counter = 0;
+	
+	void gameSet() {
 		while(true) {
 			int com = rnd.nextInt(101);
 			System.out.println("0과 100 사이의 값 중 하나를 생각하겠습니다.");
 			System.out.println("그 숫자를 6회 안에 맞추십시오.");
 			for(int i = 0; i < 6; i++) {
+				counter = i;
 				System.out.print("어떤 숫자일 것 같나요?: ");
 				you[i] = sc.nextInt();
 				if(you[i] == com) {
@@ -27,15 +38,17 @@ public class QuizHighLow {
 					System.out.println(you[i] + "은(는) 정답보다 큰 숫자입니다.");
 				}
 			}
-			System.out.println("정답은 " + com + "였습니다!");
-			System.out.println("기회를 모두 소진하셨습니다.");
+			if(you[counter] != com && counter == 5) {
+				System.out.println("정답은 " + com + "였습니다!");
+				System.out.println("기회를 모두 소진하셨습니다.");
+			}
 			System.out.print("게임을 재시작 하시겠습니까? (y/n)");
 			String str = sc.next();
 			if(!str.equalsIgnoreCase("y")) {
 				System.out.println("게임을 종료합니다.");
 				break;
 			}
-			System.out.println();
+			System.out.println();			
 		}
 	}
 }

4175c3bf7148a07dc41b699a0e3e9d92244b20c8
seungminkang@kosmo <durusk93@gmail.com>
2020-02-18 17:40:36 +0900

    선생님 풀이방법 추가

--- QuizProject/src/chapter13_1/Quiz_13_1_05.java
+++ QuizProject/src/chapter13_1/Quiz_13_1_05.java
@@ -22,6 +22,49 @@ public class Quiz_13_1_05 {
 		}else {
 			System.out.println("회문이 아닙니다.");
 		}
-		sc.close();
+
+
+		// 선생님 풀이방법
+		// 사용자한테 문자열을 입력 받아라
+		
+//		System.out.print("입력하세요 : ");
+//////	String str2 = sc.next();			// space, tab, enter 등으로 값을 구분
+//		String str = sc.nextLine();			// enter로만 값을 구분
+//		
+//		boolean b1 = true;
+//		
+//		int len = str.length();
+//		String[] arr = new String[len];
+//
+//		for(int i=0; i<len; i++) {
+//			arr[i] = str.substring(i, i+1);
+//		}
+//		
+//		String[] arr2 = new String[len];
+//		for(int i =0; i < len; i++) {
+//			arr2[len-1-i] = arr[i];
+//		}
+////		for(int i =0; i < len; i++) {
+////			System.out.print(arr2[i]);
+////		}
+//		for (int i =0; i<len; i++) {
+//			if (arr[i].compareTo(arr2[i]) != 0) // compareTo 대신 equals(arr2[i]) 를 이용하여 비교 가능
+//			{	b1 = false;
+//				break;
+//			}
+//		}
+//		
+//		// orange
+//		// egnaro		
+//		// n o
+//		// n o
+//		
+//		if(b1) {
+//			System.out.println(str + "은 회문입니다.");
+//			return;
+//		} else {
+//			System.out.println(str + "은 회문이 아닙니다.");
+//		}
+		
 	}
 }

3c9daf2db56384de595ff7f31aad1c106a30a5dc
seungminkang@kosmo <durusk93@gmail.com>
2020-02-18 17:38:09 +0900

    구구단 다른 방식 추가

--- QuizProject/src/Exam99Dan.java
+++ QuizProject/src/Exam99Dan.java
@@ -1,8 +1,16 @@
 public class Exam99Dan {
 	public static void main(String[] args) {
-		for(int i =9; i>1; i--) {			
-			for(int j = 9; j > 0; j--) {
-				System.out.print(i + " X " + j + " = " + (i*j) + "\t");
+//		for(int i =9; i>1; i--) {					// 구구단을 역순으로 출력
+//			for(int j = 9; j > 0; j--) {
+//				System.out.print(i + " X " + j + " = " + (i*j) + "\t");
+//			}
+//			System.out.println();		
+//		}
+		
+		
+		for(int i =2; i<10; i++) {					// 기본 구구단
+			for(int j = 1; j < 10; j++) {
+				System.out.printf("%d X %d = %d\t", i, j, (i*j));
 			}
 			System.out.println();		
 		}

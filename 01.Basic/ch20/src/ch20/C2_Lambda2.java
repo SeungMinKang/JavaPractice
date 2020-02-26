5e4080ef51a3c78e6a4822e8def5b877634bf762
seungminkang@kosmo <durusk93@gmail.com>
2020-02-26 18:02:56 +0900

    ch20 추가

---
+++ 01.Basic/ch20/src/ch20/C2_Lambda2.java
@@ -0,0 +1,17 @@
+package ch20;
+
+interface PrintableB {
+	void print(String s);
+}
+
+class C2_Lambda2 {
+	public static void main(String[] args) {
+		PrintableB prn = new PrintableB() {	// 익명 클래스
+			public void print(String s) {
+				System.out.println(s);
+			}
+		};
+
+		prn.print("What is Lambda?");
+	}
+}

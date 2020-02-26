4d20c6025aa4f1f100be265d796f4f619ee0df67
seungminkang@kosmo <durusk93@gmail.com>
2020-02-26 12:37:43 +0900

    ch19 추가

---
+++ 01.Basic/ch19/src/ch19/C2_AtDeprecated.java
@@ -0,0 +1,28 @@
+package ch19;
+
+interface Viewable2 {
+	@Deprecated
+	public void showIt(String str);
+	
+	public void brShowIt(String str);
+}
+
+class Viewer2 implements Viewable2 {
+	@Override
+	public void showIt(String str) {
+		System.out.println(str);
+	}
+	
+	@Override
+	public void brShowIt(String str) {
+		System.out.println('[' + str + ']');
+	}
+}
+
+class C2_AtDeprecated {
+	public static void main(String[] args) {
+		Viewable2 view = new Viewer2();
+		view.showIt("Hello Annotations");
+		view.brShowIt("Hello Annotations");
+	}
+}

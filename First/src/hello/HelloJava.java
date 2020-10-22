5cedb951fcbde54556e9195ee8f3a84ef471c5af
KangSeungMin <durusk93@gmail.com>
2020-10-22 20:18:16 +0900

    Java 복습 Chapter 1

---
+++ First/src/hello/HelloJava.java
@@ -0,0 +1,15 @@
+/*
+ 	Date: 2020년 10월 22일
+	Author: 강승민
+	Description: 자바 프로그래밍 입문 교재를 이용한 자바 개념 복습 프로젝트
+*/
+
+package hello;
+
+public class HelloJava {
+	public static void main(String[] args) {
+		// 자바 복습
+		System.out.println("Hello, Java");
+		System.out.println("My name is Seung-Min Kang");
+	}
+}
